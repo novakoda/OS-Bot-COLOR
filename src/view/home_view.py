8e4666364aaf064eb329eb8aacdfb31b9aaccddd
novakoda <novakoda9@gmail.com>
2025-07-22 13:46:42 -0600

    simplify menu ui

--- src/view/home_view.py
+++ src/view/home_view.py
@@ -22,48 +22,47 @@ class HomeView(customtkinter.CTkFrame):
         self.grid_rowconfigure(3, weight=0)  # - Warning
         self.grid_rowconfigure(5, weight=0)  # - Skip Btn
         self.grid_rowconfigure(6, weight=0)  # - Status
-        self.grid_rowconfigure(9, weight=1)  # Spacing
+        # self.grid_rowconfigure(9, weight=1)  # Spacing
 
         # Title
-        self.label_title = customtkinter.CTkLabel(self, text=f"{game_title}", font=title_font())
-        self.label_title.grid(row=1, column=0, columnspan=3, sticky="nsew", padx=15, pady=15)
+        # self.label_title = customtkinter.CTkLabel(self, text=f"{game_title}", font=title_font())
+        # self.label_title.grid(row=1, column=0, columnspan=3, sticky="nsew", padx=15, pady=15)
 
         # Description label
-        self.note = (
-            "Basic HomeViews are not fully implemented yet. Once support for non-RuneLite games is added to OSBC, this will be updated. <Game description and"
-            " user instructions here>. Click the button below to unlock the scripts."
-        )
-        self.label_note = customtkinter.CTkLabel(master=self, text=self.note, font=body_med_font())
-        self.label_note.bind(
-            "<Configure>",
-            lambda e: self.label_note.configure(wraplength=self.label_note.winfo_width() - 20),
-        )
-        self.label_note.grid(row=2, column=0, sticky="nwes", padx=15, pady=(0, 15))
+        # self.note = (
+        #     "Select a script from the sidebar"
+        # )
+        # self.label_note = customtkinter.CTkLabel(master=self, text=self.note, font=body_med_font())
+        # self.label_note.bind(
+        #     "<Configure>",
+        #     lambda e: self.label_note.configure(wraplength=self.label_note.winfo_width() - 20),
+        # )
+        # self.label_note.grid(row=2, column=0, sticky="nwes", padx=15, pady=(0, 15))
 
         # Warning label
-        self.warning = "Warning message here for extra important instructions."
-        self.label_warning = customtkinter.CTkLabel(
-            master=self,
-            text=self.warning,
-            font=body_med_font(),
-            text_color="orange",
-        )
-        self.label_warning.bind(
-            "<Configure>",
-            lambda e: self.label_warning.configure(wraplength=self.label_warning.winfo_width() - 20),
-        )
-        self.label_warning.grid(row=3, column=0, sticky="nwes", padx=15, pady=(0, 15))
+        # self.warning = "Warning message here for extra important instructions."
+        # self.label_warning = customtkinter.CTkLabel(
+        #     master=self,
+        #     text=self.warning,
+        #     font=body_med_font(),
+        #     text_color="orange",
+        # )
+        # self.label_warning.bind(
+        #     "<Configure>",
+        #     lambda e: self.label_warning.configure(wraplength=self.label_warning.winfo_width() - 20),
+        # )
+        # self.label_warning.grid(row=3, column=0, sticky="nwes", padx=15, pady=(0, 15))
 
         # Skip Btn
-        self.btn_skip = customtkinter.CTkButton(
-            master=self,
-            text="I Understand",
-            font=button_med_font(),
-            fg_color="gray40",
-            hover_color="gray25",
-            command=self.__skip,
-        )
-        self.btn_skip.grid(row=5, column=0, sticky="nwes", padx=40, pady=(0, 15))
+        # self.btn_skip = customtkinter.CTkButton(
+        #     master=self,
+        #     text="I Understand",
+        #     font=button_med_font(),
+        #     fg_color="gray40",
+        #     hover_color="gray25",
+        #     command=self.__skip,
+        # )
+        # self.btn_skip.grid(row=5, column=0, sticky="nwes", padx=40, pady=(0, 15))
 
         # Status label
         self.label_status = customtkinter.CTkLabel(master=self, text="", font=body_med_font())
